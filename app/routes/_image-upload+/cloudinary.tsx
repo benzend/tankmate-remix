2585e79f4a533cea22c70348b8cb58ab6758e051
Ben <benjamin.scottt.dev@gmail.com>
2024-10-08 21:34:30 -0600

    clean(lint): format

--- app/routes/_image-upload+/cloudinary.tsx
+++ app/routes/_image-upload+/cloudinary.tsx
@@ -1,73 +1,74 @@
 import {
-    json,
-    unstable_composeUploadHandlers as composeUploadHandlers,
-    unstable_createMemoryUploadHandler as createMemoryUploadHandler,
-    unstable_parseMultipartFormData as parseMultipartFormData,
-    ActionFunctionArgs,
-} from "@remix-run/node";
-import { uploadImage } from "#app/utils/cloudinary.server";
-import { Form, useActionData } from "@remix-run/react";
+  json,
+  unstable_composeUploadHandlers as composeUploadHandlers,
+  unstable_createMemoryUploadHandler as createMemoryUploadHandler,
+  unstable_parseMultipartFormData as parseMultipartFormData,
+  type ActionFunctionArgs,
+} from '@remix-run/node'
+import { Form, useActionData } from '@remix-run/react'
+import { uploadImage } from '#app/utils/cloudinary.server'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-    const uploadHandler = composeUploadHandlers(
-        async ({ name, data }) => {
-            if (name !== "img") {
-                return undefined
-            }
-            const uploadedImage = await uploadImage(data) as any
-            return uploadedImage.secure_url;
-        },
-        createMemoryUploadHandler()
-    );
-
-    const formData = await parseMultipartFormData(request, uploadHandler);
-    const imgSource = formData.get("img");
-    const imgDescription = formData.get("description");
-
-    if (!imgSource) {
-        return json({
-            error: "something is wrong",
-        });
+  const uploadHandler = composeUploadHandlers(async ({ name, data }) => {
+    if (name !== 'img') {
+      return undefined
     }
+    const uploadedImage = (await uploadImage(data)) as any
+    return uploadedImage.secure_url
+  }, createMemoryUploadHandler())
+
+  const formData = await parseMultipartFormData(request, uploadHandler)
+  const imgSource = formData.get('img')
+  const imgDescription = formData.get('description')
+
+  if (!imgSource) {
     return json({
-        imgSource, imgDescription
-    });
-};
+      error: 'something is wrong',
+    })
+  }
+  return json({
+    imgSource,
+    imgDescription,
+  })
+}
 
 export default function Index() {
-    const data = useActionData() as any;
-    return (
-        <>
-          <Form method="post" encType="multipart/form-data" id="upload-form">
-            <div>
-              <label htmlFor="img"> Image: </label>
-              <input id="img" type="file" name="img" accept="image/*" />
-            </div>
-            <div>
-              <label htmlFor="description"> Image description: </label>
-              <input id="description" type="text" name="description" />
-            </div>
-            <div>
-              <button type="submit"> Upload to Cloudinary </button>
-            </div>
-          </Form>
+  const data = useActionData() as any
+  return (
+    <>
+      <Form method="post" encType="multipart/form-data" id="upload-form">
+        <div>
+          <label htmlFor="img"> Image: </label>
+          <input id="img" type="file" name="img" accept="image/*" />
+        </div>
+        <div>
+          <label htmlFor="description"> Image description: </label>
+          <input id="description" type="text" name="description" />
+        </div>
+        <div>
+          <button type="submit"> Upload to Cloudinary </button>
+        </div>
+      </Form>
 
-          {data?.errorMsg && <h3>{data.errorMsg}</h3>}
-          {data?.imgSource && (
-            <>
-              <h2>Uploaded Image: </h2>
-              <img src={data.imgSource} alt={data.imgDescription || "Upload result"} />
-                <p>{data.imgDescription}</p>
-            </>
-          )}
+      {data?.errorMsg && <h3>{data.errorMsg}</h3>}
+      {data?.imgSource && (
+        <>
+          <h2>Uploaded Image: </h2>
+          <img
+            src={data.imgSource}
+            alt={data.imgDescription || 'Upload result'}
+          />
+          <p>{data.imgDescription}</p>
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export function ErrorBoundary({ error }: { error: Error }) {
-    return (
-        <div className="error-container">
-            <pre>{error.message}</pre>
-        </div>
-    );
+  return (
+    <div className="error-container">
+      <pre>{error.message}</pre>
+    </div>
+  )
 }
