9e8684122683977e2bc2d2f5cf94b90162e891a1
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2025-11-09 13:23:31 -0700

    feat: add vemetric

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,19 @@
 import {
-	json,
-	type LoaderFunctionArgs,
-	type HeadersFunction,
-	type LinksFunction,
-	type MetaFunction,
+  json,
+  type LoaderFunctionArgs,
+  type HeadersFunction,
+  type LinksFunction,
+  type MetaFunction,
 } from '@remix-run/node'
+import { VemetricScript } from '@vemetric/react';
 import {
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useLoaderData,
-	useLocation,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from '@remix-run/react'
 import { withSentry } from '@sentry/remix'
 import { HoneypotProvider } from 'remix-utils/honeypot/react'
@@ -39,195 +40,196 @@ import { makeTimings, time } from './utils/timing.server.ts'
 import { getToast } from './utils/toast.server.ts'
 
 export const links: LinksFunction = () => {
-	return [
-		// Preload svg sprite as a resource to avoid render blocking
-		{ rel: 'preload', href: iconsHref, as: 'image' },
-		{
-			rel: 'icon',
-			href: '/favicon.webp',
-			sizes: '48x48',
-		},
-		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
-		{ rel: 'apple-touch-icon', href: appleTouchIconAssetUrl },
-		{
-			rel: 'manifest',
-			href: '/site.webmanifest',
-			crossOrigin: 'use-credentials',
-		} as const, // necessary to make typescript happy
-		{ rel: 'stylesheet', href: tailwindStyleSheetUrl },
-	].filter(Boolean)
+  return [
+    // Preload svg sprite as a resource to avoid render blocking
+    { rel: 'preload', href: iconsHref, as: 'image' },
+    {
+      rel: 'icon',
+      href: '/favicon.webp',
+      sizes: '48x48',
+    },
+    { rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+    { rel: 'apple-touch-icon', href: appleTouchIconAssetUrl },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+      crossOrigin: 'use-credentials',
+    } as const, // necessary to make typescript happy
+    { rel: 'stylesheet', href: tailwindStyleSheetUrl },
+  ].filter(Boolean)
 }
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
-	return [
-		{ title: data ? 'TankMate' : 'Error | TankMate' },
-		{ name: 'description', content: `Monitor and maintain your aquarium's health with AI-powered coral analysis, water quality tracking, and smart alerts. TankMate helps reef enthusiasts keep their marine ecosystems thriving.` },
-	]
+  return [
+    { title: data ? 'TankMate' : 'Error | TankMate' },
+    { name: 'description', content: `Monitor and maintain your aquarium's health with AI-powered coral analysis, water quality tracking, and smart alerts. TankMate helps reef enthusiasts keep their marine ecosystems thriving.` },
+  ]
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const timings = makeTimings('root loader')
-	const userId = await time(() => getUserId(request), {
-		timings,
-		type: 'getUserId',
-		desc: 'getUserId in root',
-	})
+  const timings = makeTimings('root loader')
+  const userId = await time(() => getUserId(request), {
+    timings,
+    type: 'getUserId',
+    desc: 'getUserId in root',
+  })
 
-	const user = userId
-		? await time(
-				() =>
-					prisma.user.findUniqueOrThrow({
-						select: {
-							id: true,
-							name: true,
-							username: true,
-							image: { select: { id: true } },
-							roles: {
-								select: {
-									name: true,
-									permissions: {
-										select: { entity: true, action: true, access: true },
-									},
-								},
-							},
-						},
-						where: { id: userId },
-					}),
-				{ timings, type: 'find user', desc: 'find user in root' },
-			)
-		: null
-	if (userId && !user) {
-		console.info('something weird happened')
-		// something weird happened... The user is authenticated but we can't find
-		// them in the database. Maybe they were deleted? Let's log them out.
-		await logout({ request, redirectTo: '/' })
-	}
-	const { toast, headers: toastHeaders } = await getToast(request)
-	const honeyProps = honeypot.getInputProps()
+  const user = userId
+    ? await time(
+      () =>
+        prisma.user.findUniqueOrThrow({
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            image: { select: { id: true } },
+            roles: {
+              select: {
+                name: true,
+                permissions: {
+                  select: { entity: true, action: true, access: true },
+                },
+              },
+            },
+          },
+          where: { id: userId },
+        }),
+      { timings, type: 'find user', desc: 'find user in root' },
+    )
+    : null
+  if (userId && !user) {
+    console.info('something weird happened')
+    // something weird happened... The user is authenticated but we can't find
+    // them in the database. Maybe they were deleted? Let's log them out.
+    await logout({ request, redirectTo: '/' })
+  }
+  const { toast, headers: toastHeaders } = await getToast(request)
+  const honeyProps = honeypot.getInputProps()
 
-	return json(
-		{
-			user,
-			requestInfo: {
-				hints: getHints(request),
-				origin: getDomainUrl(request),
-				path: new URL(request.url).pathname,
-				userPrefs: {
-					theme: getTheme(request),
-				},
-			},
-			ENV: getEnv(),
-			toast,
-			honeyProps,
-		},
-		{
-			headers: combineHeaders(
-				{ 'Server-Timing': timings.toString() },
-				toastHeaders,
-			),
-		},
-	)
+  return json(
+    {
+      user,
+      requestInfo: {
+        hints: getHints(request),
+        origin: getDomainUrl(request),
+        path: new URL(request.url).pathname,
+        userPrefs: {
+          theme: getTheme(request),
+        },
+      },
+      ENV: getEnv(),
+      toast,
+      honeyProps,
+    },
+    {
+      headers: combineHeaders(
+        { 'Server-Timing': timings.toString() },
+        toastHeaders,
+      ),
+    },
+  )
 }
 
 export const headers: HeadersFunction = ({ loaderHeaders }) => {
-	const headers = {
-		'Server-Timing': loaderHeaders.get('Server-Timing') ?? '',
-	}
-	return headers
+  const headers = {
+    'Server-Timing': loaderHeaders.get('Server-Timing') ?? '',
+  }
+  return headers
 }
 
 function Document({
-	children,
-	nonce,
-	theme = 'light',
-	env = {},
-	allowIndexing = true,
+  children,
+  nonce,
+  theme = 'light',
+  env = {},
+  allowIndexing = true,
 }: {
-	children: React.ReactNode
-	nonce: string
-	theme?: Theme
-	env?: Record<string, string>
-	allowIndexing?: boolean
+  children: React.ReactNode
+  nonce: string
+  theme?: Theme
+  env?: Record<string, string>
+  allowIndexing?: boolean
 }) {
-	return (
-		<html lang="en" className={`${theme} h-full overflow-x-hidden`}>
-			<head>
-				<ClientHintCheck nonce={nonce} />
-				<Meta />
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				{allowIndexing ? null : (
-					<meta name="robots" content="noindex, nofollow" />
-				)}
-				<Links />
-			</head>
-			<body className="bg-background text-foreground">
-				{children}
-				<script
-					nonce={nonce}
-					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(env)}`,
-					}}
-				/>
-				<ScrollRestoration nonce={nonce} />
-				<Scripts nonce={nonce} />
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en" className={`${theme} h-full overflow-x-hidden`}>
+      <head>
+        <ClientHintCheck nonce={nonce} />
+        <Meta />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {allowIndexing ? null : (
+          <meta name="robots" content="noindex, nofollow" />
+        )}
+        <Links />
+      </head>
+      <body className="bg-background text-foreground">
+        <VemetricScript token={process.env.VEMETRIC_TOKEN || 'nada'}></VemetricScript>
+        {children}
+        <script
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(env)}`,
+          }}
+        />
+        <ScrollRestoration nonce={nonce} />
+        <Scripts nonce={nonce} />
+      </body>
+    </html>
+  )
 }
 
 function App() {
-	const data = useLoaderData<typeof loader>()
-	const nonce = useNonce()
-	const theme = useTheme()
-	const allowIndexing = data.ENV.ALLOW_INDEXING !== 'false'
-	const location = useLocation()
-	useToast(data.toast)
+  const data = useLoaderData<typeof loader>()
+  const nonce = useNonce()
+  const theme = useTheme()
+  const allowIndexing = data.ENV.ALLOW_INDEXING !== 'false'
+  const location = useLocation()
+  useToast(data.toast)
 
-	return (
-		<Document
-			nonce={nonce}
-			theme={theme}
-			allowIndexing={allowIndexing}
-			env={data.ENV}
-		>
-			{!location.pathname.includes('/dashboard') && <MarketingNav />}
-			<div className="flex-1">
-				<Outlet />
-			</div>
-			{!location.pathname.includes('/dashboard') && <MarketingFooter />}
-			<EpicToaster closeButton position="top-center" theme={theme} />
-			<EpicProgress />
-		</Document>
-	)
+  return (
+    <Document
+      nonce={nonce}
+      theme={theme}
+      allowIndexing={allowIndexing}
+      env={data.ENV}
+    >
+      {!location.pathname.includes('/dashboard') && <MarketingNav />}
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      {!location.pathname.includes('/dashboard') && <MarketingFooter />}
+      <EpicToaster closeButton position="top-center" theme={theme} />
+      <EpicProgress />
+    </Document>
+  )
 }
 
 function AppWithProviders() {
-	const data = useLoaderData<typeof loader>()
-	return (
-		<HoneypotProvider {...data.honeyProps}>
-			<App />
-		</HoneypotProvider>
-	)
+  const data = useLoaderData<typeof loader>()
+  return (
+    <HoneypotProvider {...data.honeyProps}>
+      <App />
+    </HoneypotProvider>
+  )
 }
 
 export default withSentry(AppWithProviders)
 
 export function ErrorBoundary() {
-	// the nonce doesn't rely on the loader so we can access that
-	const nonce = useNonce()
+  // the nonce doesn't rely on the loader so we can access that
+  const nonce = useNonce()
 
-	// NOTE: you cannot use useLoaderData in an ErrorBoundary because the loader
-	// likely failed to run so we have to do the best we can.
-	// We could probably do better than this (it's possible the loader did run).
-	// This would require a change in Remix.
+  // NOTE: you cannot use useLoaderData in an ErrorBoundary because the loader
+  // likely failed to run so we have to do the best we can.
+  // We could probably do better than this (it's possible the loader did run).
+  // This would require a change in Remix.
 
-	// Just make sure your root route never errors out and you'll always be able
-	// to give the user a better UX.
+  // Just make sure your root route never errors out and you'll always be able
+  // to give the user a better UX.
 
-	return (
-		<Document nonce={nonce}>
-			<GeneralErrorBoundary />
-		</Document>
-	)
+  return (
+    <Document nonce={nonce}>
+      <GeneralErrorBoundary />
+    </Document>
+  )
 }
--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod'
 
 const schema = z.object({
-	NODE_ENV: z.enum(['production', 'development', 'test'] as const),
-	DATABASE_PATH: z.string(),
-	DATABASE_URL: z.string(),
-	SESSION_SECRET: z.string(),
-	INTERNAL_COMMAND_TOKEN: z.string(),
-	HONEYPOT_SECRET: z.string(),
+  NODE_ENV: z.enum(['production', 'development', 'test'] as const),
+  DATABASE_PATH: z.string(),
+  DATABASE_URL: z.string(),
+  SESSION_SECRET: z.string(),
+  INTERNAL_COMMAND_TOKEN: z.string(),
+  HONEYPOT_SECRET: z.string(),
   OPENAI_API_KEY: z.string().optional(),
-	CACHE_DATABASE_PATH: z.string(),
+  CACHE_DATABASE_PATH: z.string(),
 
   // images
   CLOUDINARY_CLOUD_NAME: z.string().optional(),
@@ -16,34 +16,36 @@ const schema = z.object({
   CLOUDINARY_API_SECRET: z.string().optional(),
   UPLOADTHING_TOKEN: z.string().optional(),
 
-	// If you plan on using Sentry, uncomment this line
-	// SENTRY_DSN: z.string(),
-	// If you plan to use Resend, uncomment this line
-	RESEND_API_KEY: z.string().default('MOCK_RESEND_API_KEY'),
-	// If you plan to use GitHub auth, remove the default:
-	GITHUB_CLIENT_ID: z.string().default('MOCK_GITHUB_CLIENT_ID'),
-	GITHUB_CLIENT_SECRET: z.string().default('MOCK_GITHUB_CLIENT_SECRET'),
-	GITHUB_TOKEN: z.string().default('MOCK_GITHUB_TOKEN'),
-	ALLOW_INDEXING: z.enum(['true', 'false']).optional(),
+  VEMETRIC_TOKEN: z.string().optional(),
+
+  // If you plan on using Sentry, uncomment this line
+  // SENTRY_DSN: z.string(),
+  // If you plan to use Resend, uncomment this line
+  RESEND_API_KEY: z.string().default('MOCK_RESEND_API_KEY'),
+  // If you plan to use GitHub auth, remove the default:
+  GITHUB_CLIENT_ID: z.string().default('MOCK_GITHUB_CLIENT_ID'),
+  GITHUB_CLIENT_SECRET: z.string().default('MOCK_GITHUB_CLIENT_SECRET'),
+  GITHUB_TOKEN: z.string().default('MOCK_GITHUB_TOKEN'),
+  ALLOW_INDEXING: z.enum(['true', 'false']).optional(),
 })
 
 declare global {
-	namespace NodeJS {
-		interface ProcessEnv extends z.infer<typeof schema> {}
-	}
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof schema> { }
+  }
 }
 
 export function init() {
-	const parsed = schema.safeParse(process.env)
+  const parsed = schema.safeParse(process.env)
 
-	if (parsed.success === false) {
-		console.error(
-			'❌ Invalid environment variables:',
-			parsed.error.flatten().fieldErrors,
-		)
+  if (parsed.success === false) {
+    console.error(
+      '❌ Invalid environment variables:',
+      parsed.error.flatten().fieldErrors,
+    )
 
-		throw new Error('Invalid environment variables')
-	}
+    throw new Error('Invalid environment variables')
+  }
 }
 
 /**
@@ -56,18 +58,18 @@ export function init() {
  * @returns all public ENV variables
  */
 export function getEnv() {
-	return {
-		MODE: process.env.NODE_ENV,
-		SENTRY_DSN: process.env.SENTRY_DSN,
-		ALLOW_INDEXING: process.env.ALLOW_INDEXING,
-	}
+  return {
+    MODE: process.env.NODE_ENV,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    ALLOW_INDEXING: process.env.ALLOW_INDEXING,
+  }
 }
 
 type ENV = ReturnType<typeof getEnv>
 
 declare global {
-	var ENV: ENV
-	interface Window {
-		ENV: ENV
-	}
+  var ENV: ENV
+  interface Window {
+    ENV: ENV
+  }
 }
